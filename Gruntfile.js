c9e34d5ec371279f1e51dd1b7ed520ce06a6183c
Rishi Shah <rishihahs@gmail.com>
2013-11-06 21:45:59 -0600

    Organization

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,31 +13,13 @@ module.exports = function(grunt) {
                     optimize: "none",
                     modules: [
                         {
-                            name: 'user/iris-chat-user',
-                            include: ['deps/almond']
-                        },
-
-                        {
-                            name: 'representative/iris-chat-representative',
+                            name: 'chat/chat',
                             include: ['deps/almond']
                         }
                     ],
                 }
             }
         },
-
-        copy: {
-            main: {
-                files: [{
-                    expand: true,
-                    flatten: true,
-                    src: ['release/build/user/iris-chat-user.js', 'release/build/representative/iris-chat-representative.js'],
-                    dest: 'release/'
-                }]
-            }
-        },
-
-        clean: ["release/build"],
 
         uglify: {
             options: {
@@ -46,24 +28,21 @@ module.exports = function(grunt) {
             main: {
                 files: [
                     {
-                        src: ['release/iris-chat-user.js', 'lib/user/run.js'],
-                        dest: 'release/iris-chat-user.js'
-                    },
-
-                    {
-                        src: ['release/iris-chat-representative.js', 'lib/representative/run.js'],
-                        dest: 'release/iris-chat-representative.js'
+                        src: ['release/build/chat/chat.js', 'lib/chat/run.js'],
+                        dest: 'release/iris.js'
                     }
                 ]
             }
         },
+
+        clean: ["release/build"],
 
         compass: {
             compile: {
                 options: {
                     sassDir: 'stylesheets',
                     cssDir: 'release',
-                    specify: ['stylesheets/iris-chat-styles.scss'],
+                    specify: ['stylesheets/iris-styles.scss'],
                     environment: 'production'
                 }
             }
@@ -72,12 +51,11 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "beautify" task.
     grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-compass');
 
     // Default task(s).
-    grunt.registerTask('default', ['requirejs', 'copy', 'clean', 'uglify', 'compass']);
+    grunt.registerTask('default', ['requirejs', 'uglify', 'clean', 'compass']);
 
 };
