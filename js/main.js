6b9e1ea403149b2c1d78149e99673819ea27bbf9
Rishi Shah <rishihahs@gmail.com>
2013-11-09 14:16:04 -0600

    Use heroku iris

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('http://irisapp.herokuapp.com');
 
     socket.on('connect', function() {
         // socket connected
